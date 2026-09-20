3b4f754df2f799f7fcc86665e0a084435f9a5b40
hemkeshv <hemkeshv@gmail.com>
2026-09-20 13:00:10 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10
+print(type(variable))
+
+variable = 98.6
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "string"
+print(type(variable))
+
+variable = [1,2,3]
+print(type(variable))
